71d3833a6d093a9e86b5525c09c017c66dc7f885
LucioFider <fidelislucio404@gmail.com>
2023-01-07 09:29:00 +1000

    3rd-commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,13 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Cart from "./components/Cart";
+import ProductList from "./components/ProductList";
+import { useState } from "react";
+
 function App() {
+  const [viewCart, setViewCart] = useState<boolean>(false);
+  const pageContent = viewCart ? <Cart /> : <ProductList />;
+
   return (
     <div className="">
       <h1>Shopping Cart React + TypeScript</h1>
